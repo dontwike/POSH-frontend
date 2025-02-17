8d86a2e148bce3c7b34f28a13c4c68659b206852
Pranjal Goyal <100211916+dontwike@users.noreply.github.com>
2025-02-17 14:31:36 +0530

    Shop fix

--- src/components/Shop/ShopCollection.jsx
+++ src/components/Shop/ShopCollection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { shopData } from "../../Data";
 
-const ShopCollection = (props) => {
+const ShopCollection = () => {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-6">
       {shopData.map((product) => (
@@ -9,7 +9,7 @@ const ShopCollection = (props) => {
           <img
             src={product.img}
             alt={product.name}
-            className="w-full h-72 lg:h-96 object-cover"
+            className="w-full h-72 lg:h-auto object-contain"
           />
           <div className="py-3">
             <h3 className="text-gray-600">{product.name}</h3>
