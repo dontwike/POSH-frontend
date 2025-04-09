1429788aa897faf1ff0f03d0b703e444c25062fe
Pranjal Goyal <100211916+dontwike@users.noreply.github.com>
2025-04-09 22:34:06 +0530

    error resolved

--- src/components/global/SubHeading.jsx
+++ src/components/global/SubHeading.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const SubHeading = ({ heading }) => {
+const SubHeading = (props) => {
   return (
-    <h1 className="text-xl md:text-2xl lg:text-3xl uppercase mb-4">{heading}</h1>
+    <h1 className="text-xl md:text-2xl lg:text-3xl uppercase mb-4">{props.subheading}</h1>
   );
 };
 
